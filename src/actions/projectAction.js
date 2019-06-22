b309b190f338f9b69caaa28863d220b18c94d027
mickey english <mickeyeng92@gmail.com>
2019-06-22 15:06:39 +0100

    create project working with firestore

--- src/actions/projectAction.js
+++ src/actions/projectAction.js
@@ -1,6 +1,20 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make asynk call to database
+        // Fire Store
+        const firestore = getFirestore()
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'mickey',
+            authorLastName: 'english',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project })
+        }).catch((error) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', error })
+        })
+
         dispatch({
             type: 'CREATE_PROJECT',
             project
--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -10,9 +10,12 @@ const projectReducer = (state = defaultState, action) => {
     switch (action.type) {
         case 'CREATE_PROJECT':
             console.log('created project', action.project)
+            return state
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.error)
+        default:
+            return state
     }
-
-    return state
 };
 
 export default projectReducer;
