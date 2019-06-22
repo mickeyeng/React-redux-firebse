770898a920302c30e036dbaffdf7db858e0bf6ad
mickey english <mickeyeng92@gmail.com>
2019-06-22 14:36:02 +0100

    Changed package.json project name

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCb4yrFg6fWpxGl4OXN2kgPAdK0q_CoWkU",
+    authDomain: "mickey-react-redux-firebase.firebaseapp.com",
+    databaseURL: "https://mickey-react-redux-firebase.firebaseio.com",
+    projectId: "mickey-react-redux-firebase",
+    storageBucket: "",
+    messagingSenderId: "169155060482",
+    appId: "1:169155060482:web:061538ffb85ed9de"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase
