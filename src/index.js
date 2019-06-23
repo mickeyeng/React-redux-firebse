7772ae0527849fc2882f4c2b3883d204317e8195
mickey english <mickeyeng92@gmail.com>
2019-06-23 19:50:37 +0100

    added a function to check if firebase is ready before reac renders to the dom

--- src/index.js
+++ src/index.js
@@ -16,11 +16,16 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
+        reactReduxFirebase(fbConfig, { attachAuthIsReady: true }) // auth will check if firebase is ready
     )
 )
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// React won't load until firebase is ready!
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+})
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
